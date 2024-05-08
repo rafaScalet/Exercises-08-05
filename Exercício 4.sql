97721cb2642fb0aec8da63d227a6460d5d63de40
rafaScalet <rafaelscalet2018@gmail.com>
2024-05-08 18:26:21 -0300

    Exercise 4

--- Exercício 4.sql
+++ Exercício 4.sql
@@ -2,6 +2,6 @@ use empresa
 select c1.Nome, c1.Sobrenome
 from Clientes as c1
 inner join Clientes as c2
-on (c1.Sobrenome = c2.Sobrenome or c1.Nome = c2.Sobrenome) and c1.ID <> c2.ID
+on (c1.Sobrenome = c2.Sobrenome or c1.Nome = c2.Nome) and c1.ID <> c2.ID
 order by c1.Sobrenome, c1.Nome
 go
